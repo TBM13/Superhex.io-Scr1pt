76d666b546c7782a32b2ae93b2acdba06428d2c5
TBM13 <mateo.trueba@gmail.com>
2022-02-27 13:25:23 -0300

    Refactoring

--- Script.js
+++ Script.js
@@ -14,10 +14,11 @@
 // ==/UserScript==
 
 var style = document.createElement("style"),
-    AdsTBM = localStorage.getItem("AdsTBM"), currSkin = localStorage.getItem("selectedSkin"), currQuality = localStorage.getItem("quality"), zoomHack = localStorage.getItem("zoomTBM"), zoomValue = localStorage.getItem("zoomValTBM"),
+    AdsTBM = localStorage.getItem("AdsTBM"), currQuality = localStorage.getItem("quality"), zoomHack = localStorage.getItem("zoomTBM"), zoomValue = localStorage.getItem("zoomValTBM"),
     superhex = window.superhex,
     stopRemoveAdsService = false,
-    math_max_o = Math.max;
+    customQualityButton,
+    originalMathMax = Math.max, originalOnMouseWheel = window.onmousewheel;
 
 style.innerHTML = '.scr1ptPanel {background:rgba(0,60,0,0.5); border-style: solid; border-width: 3px; border-color: rgb(60,185,60,0.5); border-radius: 5px;} .scr1ptButton {line-height: 1; outline: none; color: white; background-color: #5CB85C; border-radius: 4px; border-width: 0px; transition: 0.2s;} .scr1ptButton:hover {background-color: #5ed15e; cursor: pointer;} .scr1ptButton:active {background-color: #4e9c4e;} .scr1ptButton.unselected {opacity: 0.5;} .scr1ptButton .spinner {display: none; vertical-align: middle;} .scr1ptButton.button-loading {background-color: #7D7D7D; color: white;} .scr1ptButton.button-loading .spinner {display: inline-block;} .scr1ptButton-grey {color: black; background-color: #f5f5f5;} .scr1ptButton-grey:hover {background-color: white; color: #5e5e5e;} .scr1ptButton-grey:active {background-color: #cccccc; color: #5e5e5e;} .scr1ptButton-gold {background-color: #c9c818;} .scr1ptButton-gold:hover {background-color: #d9d71a;} .scr1ptButton-gold:active {background-color: #aba913;}';
 document.getElementsByTagName("head")[0].appendChild(style);
@@ -26,32 +27,43 @@ function init() {
     createGui();
 
     if (AdsTBM) removeAdsService();
-    changeQuality(currQuality == null ? 0.75 : currQuality);
+
     window.zoomValue = zoomValue ? Number(zoomValue) : 13;
-    if (zoomHack == "True") zoomH(false);
+    if (zoomHack == "True") toggleZoomHack(true);
 
-    let highQB = document.getElementById("button-quality-high");
-    let mediumQB = document.getElementById("button-quality-medium");
-    let lowQB = document.getElementById("button-quality-low");
-
-    highQB.onclick = () => changeQuality(1);
-    highQB.setAttribute("class", highQB.className == "green" ? "scr1ptButton" : "scr1ptButton unselected");
-    mediumQB.onclick = () => changeQuality(0.75);
-    mediumQB.setAttribute("class", mediumQB.className == "green" ? "scr1ptButton" : "scr1ptButton unselected");
-    lowQB.onclick = () => changeQuality(0.5);
-    lowQB.setAttribute("class", lowQB.className == "green" ? "scr1ptButton" : "scr1ptButton unselected");
+    changeQuality(currQuality ?? 0.75);
+    document.getElementById("button-quality-high").onclick = () => changeQuality(1);
+    document.getElementById("button-quality-medium").onclick = () => changeQuality(0.75);
+    document.getElementById("button-quality-low").onclick = () => changeQuality(0.5);
 }
 
 function changeQuality(qualityValue) {
     superhex.setQuality(qualityValue);
+
     currQuality = localStorage.getItem("quality");
-    document.getElementById("btn2").innerText = "Custom Quality";
+    customQualityButton.innerText = "Custom Quality";
+
     if (currQuality != 1 && currQuality != 0.75 && currQuality != 0.5) {
-        document.getElementById("btn2").setAttribute("class", "scr1ptButton");
-        document.getElementById("btn2").innerText += " (" + currQuality.toString() + ")";
-    } else {
-        document.getElementById("btn2").setAttribute("class", "scr1ptButton unselected");
+        customQualityButton.className = "scr1ptButton";
+        customQualityButton.innerText += " (" + currQuality + ")";
+        return;
     }
+
+    customQualityButton.className = "scr1ptButton unselected";
+}
+
+function customQuality() {
+    let QualityPrompt = Number(window.prompt("Insert value. Example:\n0.25: Very low\n0.5: Low\n0.75: Medium\n1: High\n1.5: Very high\n2: Ultra"));
+    if (QualityPrompt > 2.7) alert("WARNING: Values higher than 2.7 may cause problems.");
+    if (QualityPrompt < 0.1 && QualityPrompt > 0) alert("WARNING: Values lower may 0.1 can cause problems.");
+
+    if (QualityPrompt.toString() == "NaN") {
+        alert("Invalid value. Make sure to only use numbers.\nExample: 1.2"); 
+        return
+    }
+
+    if (QualityPrompt == 0) return;
+    changeQuality(QualityPrompt);
 }
 
 function toggleRemoveAds(restoreAds) {
@@ -79,15 +91,13 @@ function removeAdsService(timeout = 50) {
         timeout = 50;
     }
 
-    console.log(timeout);
     if (timeout < 1000) timeout += 50;
-
     if (!stopRemoveAdsService) setTimeout(() => removeAdsService(timeout), timeout);
 }
 
 function removeAdElement(elem) {
     elem.innerHTML = "";
-    elem.setAttribute("style", "display: none;");
+    elem.style.display = "none";
 }
 
 var originalKeyup = document.onkeyup;
@@ -118,57 +128,48 @@ document.onkeyup = (e) => {
     }
 };
 
-function changeQ() {
-    var QualityPrompt = Number(window.prompt("Insert value. Example:\n0.25: Very low\n0.5: Low\n0.75: Medium\n1: High\n1.5: Very high\n2: Ultra"));
-    if (QualityPrompt > 2.7) alert("WARNING: Values higher than 2.7 can cause problems.");
-    if (QualityPrompt < 0.1 && QualityPrompt > 0) alert("WARNING: Values lower than 0.1 can cause problems.");
-    if (QualityPrompt.toString() == "NaN") alert("Invalid value. Make sure to only use numbers.\nExample: 1.2"); else {
-        if (QualityPrompt === 0) return;
-        changeQuality(QualityPrompt);
-        alert("Quality changed to: " + QualityPrompt);
+function unlockSkins() {
+    let skins = ["shareClicked", "subscribeClicked", "likeClicked", "tweetClicked", "followClicked"]
+
+    let unlocked = 0;
+    skins.forEach((skin) => {
+        if (localStorage.getItem(skin) == 1) unlocked++;
+    });
+
+    if (unlocked == skins.length) { 
+        alert("You have already unlocked all the skins."); 
+        return;
     }
+
+    skins.forEach((skin) => {
+        localStorage.setItem(skin, 1);
+    });
+
+    superhex.nextSkins();
+    superhex.previousSkins();
+    alert("All the skins were unlocked!");
 }
 
-function unlockSK() {
-    if (localStorage.getItem("shareClicked") && localStorage.getItem("subscribeClicked") && localStorage.getItem("likeClicked") && localStorage.getItem("tweetClicked") && localStorage.getItem("followClicked")) alert("You already unlocked the skins."); else {
-        var ChickenS = true, TweetS = true, CowS = true, RedBirdS = true, ElephantS = true;
-        if (localStorage.getItem("followClicked")) ChickenS = false;
-        if (localStorage.getItem("tweetClicked")) TweetS = false;
-        if (localStorage.getItem("likeClicked")) CowS = false;
-        if (localStorage.getItem("subscribeClicked")) RedBirdS = false;
-        if (localStorage.getItem("shareClicked")) ElephantS = false;
-        localStorage.setItem("shareClicked", 1);
-        localStorage.setItem("subscribeClicked", 1);
-        localStorage.setItem("likeClicked", 1);
-        localStorage.setItem("tweetClicked", 1);
-        localStorage.setItem("followClicked", 1);
-        superhex.nextSkins();
-        superhex.previousSkins();
-        var msg = "The following skins were unlocked:";
-        if (ChickenS) msg += "\nYellow chicken";
-        if (TweetS) msg += "\nLight blue bird";
-        if (CowS) msg += "\nCow";
-        if (RedBirdS) msg += "\nRed bird";
-        if (ElephantS) msg += "\nElephant";
-        alert(msg);
-    }
-}
-
-function zoomH(message) {
-    if (zoomHack == "True" && message) {
+function toggleZoomHack(enable) {
+    if (!enable) {
         localStorage.removeItem("zoomTBM");
-        Math.max = math_max_o;
+        Math.max = originalMathMax;
+        window.onmousewheel = originalOnMouseWheel;
     } else {
         Math.max = function (a, b) {
-            return a == window.innerWidth / 40 / 2 / .75 && b == window.innerHeight / 40 / Math.sqrt(3) ? window.zoomValue : math_max_o(a, b);
+            return a == window.innerWidth / 40 / 2 / .75 && b == window.innerHeight / 40 / Math.sqrt(3) ? window.zoomValue : originalMathMax(a, b);
         };
-        if (message) localStorage.setItem("zoomTBM", "True");
-        document.onmousewheel = function(e) {
-            var delta;
+
+        localStorage.setItem("zoomTBM", "True");
+
+        window.onmousewheel = function(e) {
+            if (originalOnMouseWheel) originalOnMouseWheel();
+
+            let delta;
             if (!e) e = window.event;
             if (e.wheelDelta) delta = e.wheelDelta / 60; else if (e.detail) delta = -e.detail / 2;
 
-            var oldValue = window.zoomValue;
+            let oldValue = window.zoomValue;
             if (delta !== null && delta > 0) {
                if (window.zoomValue < 60) window.zoomValue += window.zoomValue < 16 ? 1 : 2;
             } else {
@@ -181,18 +182,100 @@ function zoomH(message) {
             }
         };
     }
+
     zoomHack = localStorage.getItem("zoomTBM");
 }
 
-function setZoomH() {
-    var zoomHPrompt = window.prompt("Insert zoom value.\nBy default is 13 (higher value = more zoom)\nNote: You can also use the mouse wheel to zoom in/out.");
+function setZoomHackValue() {
+    let zoomHPrompt = window.prompt("Insert zoom value.\nBy default is 13 (higher value = more zoom)\nNote: You can also use the mouse wheel to zoom in/out.");
+
     if (zoomHPrompt !== null && zoomHPrompt.length != 0) {
         zoomHPrompt = Number(zoomHPrompt);
-        if (zoomHPrompt > 60) alert("Value can't be greater than 60."); else if (zoomHPrompt < 5) alert("Value can't be less than 5."); else if (zoomHPrompt.toString() == "NaN") alert("Invalid value. Make sure to only use numbers."); else {
+        if (zoomHPrompt > 60) alert("Value can't be greater than 60."); 
+        else if (zoomHPrompt < 5) alert("Value can't be less than 5."); 
+        else if (zoomHPrompt.toString() == "NaN") alert("Invalid value. Make sure to only use numbers."); 
+        else {
             window.zoomValue = zoomHPrompt;
             localStorage.setItem("zoomValTBM", zoomHPrompt);
         }
     }
+}
+
+function createGui() {
+    let homepage = document.getElementById("homepage");
+
+    let panel = new ModPanel(homepage);
+    let mainPanel = panel.mainPanel;
+
+    mainPanel.className = "scr1ptPanel";
+    mainPanel.style.position = "fixed";
+    mainPanel.style.top = "12%";
+    mainPanel.style.left = "-4px";
+    mainPanel.style.width = "20%";
+    mainPanel.style.maxWidth = "200px";
+    mainPanel.style.height = "60%";
+
+    let title = document.createElement("h3");
+    title.style.color = "white";
+    title.style.position = "fixed";
+    title.style.top = "6%";
+    title.style.left = "5px";
+    title.innerText = "Superhex.io Scr1pt";
+    homepage.appendChild(title);
+
+    let githubButton = panel.createButton("GitHub");
+    githubButton.className = "scr1ptButton scr1ptButton-gold";
+    githubButton.onclick = () => window.open("https://github.com/TBM13/Superhex.io-Scr1pt");
+
+    let greasyForkButton = panel.createButton("Greasy Fork");
+    greasyForkButton.className = "scr1ptButton scr1ptButton-gold";
+    greasyForkButton.onclick = () => window.open("https://greasyfork.org/es/scripts/36071-superhex-io-scr1pt");
+
+    let unlockSkinsButton = panel.createButton("Unlock skins");
+    unlockSkinsButton.className = "scr1ptButton";
+    unlockSkinsButton.onclick = () => unlockSkins();
+
+    customQualityButton = document.createElement("Button");
+    customQualityButton.setAttribute("class", "scr1ptButton unselected");
+    customQualityButton.setAttribute("type", "button");
+    customQualityButton.setAttribute("id", "btn2");
+    customQualityButton.innerText = "Custom Quality";
+    customQualityButton.onclick = () => customQuality();
+    document.getElementById("button-quality-high").parentElement.appendChild(customQualityButton);
+
+    let versionText = document.createElement("h5");
+    versionText.setAttribute("style", "color: rgba(255,255,255,0.6); position: absolute; bottom: -20px; right: 5px;");
+    versionText.innerText = "v1.9.2";
+    mainPanel.appendChild(versionText);
+
+    let removeAdsCheckbox = panel.createCheckbox("Remove ads")[0]
+    removeAdsCheckbox.onclick = () => toggleRemoveAds(!removeAdsCheckbox.checked);
+    removeAdsCheckbox.checked = AdsTBM;
+
+    let zoomHackCheckbox = panel.createCheckbox("Zoom Hack")[0];
+    zoomHackCheckbox.onclick = () => toggleZoomHack(zoomHackCheckbox.checked);
+    zoomHackCheckbox.checked = zoomHack == "True";
+
+    let btnZHS = document.createElement("Button");
+    btnZHS.setAttribute("style", "position: absolute; top: 286px; left: 130px; height: 16px; width: 16px;");
+    btnZHS.setAttribute("class", "scr1ptButton");
+    btnZHS.innerHTML = "<img src='https://lh3.googleusercontent.com/Abm4DjvPOP55GK2MCe9gYh8M1ZJa7ws71oXcW2q6Rl1pQXIQ_bUcVxbN5vZ8_6pmP248O-uQEN2fUxq-xzFlzefdXyEBakvzEgGKzIwSkcdSBHdM2PwtgpgXbMvbP_N7FSI4BYIujg=s16-no' style='position: absolute; left: 0px; top: 0px;'/>";
+    btnZHS.setAttribute("type", "button");
+    btnZHS.setAttribute("id", "btnZHS");
+    btnZHS.onclick = () => setZoomHackValue();
+    mainPanel.appendChild(btnZHS);
+
+    let hotkeysPanel = document.createElement("Div");
+    hotkeysPanel.setAttribute("style", "position: fixed; bottom: -4px; right: -4px; height:150px; width:300px;");
+    hotkeysPanel.setAttribute("class", "scr1ptPanel");
+    hotkeysPanel.setAttribute("id", "scr1ptPanel2");
+    document.getElementById("homepage").appendChild(hotkeysPanel);
+
+    let scrText2 = document.createElement("h4");
+    scrText2.setAttribute("style", "color: white; position: relative; left: 10px;");
+    scrText2.setAttribute("id", "scrText2");
+    scrText2.innerText = "Hotkeys:\n\n1 = Hide/show Leaderboard.\n0 = Hide/show UI.\n2 = Hide/show FPS and connection info.";
+    hotkeysPanel.appendChild(scrText2);
 }
 
 class ModPanel {
@@ -245,83 +328,6 @@ class ModPanel {
 
         return [check, label];
     }
-}
-
-function createGui() {
-    let homepage = document.getElementById("homepage");
-
-    let panel = new ModPanel(homepage);
-    let mainPanel = panel.mainPanel;
-
-    mainPanel.className = "scr1ptPanel";
-    mainPanel.style.position = "fixed";
-    mainPanel.style.top = "12%";
-    mainPanel.style.left = "-4px";
-    mainPanel.style.width = "20%";
-    mainPanel.style.maxWidth = "200px";
-    mainPanel.style.height = "60%";
-
-    let title = document.createElement("h3");
-    title.style.color = "white";
-    title.style.position = "fixed";
-    title.style.top = "6%";
-    title.style.left = "5px";
-    title.innerText = "Superhex.io Scr1pt";
-    homepage.appendChild(title);
-
-    let githubButton = panel.createButton("GitHub");
-    githubButton.className = "scr1ptButton scr1ptButton-gold";
-    githubButton.onclick = () => window.open("https://github.com/TBM13/Superhex.io-Scr1pt");
-
-    let greasyForkButton = panel.createButton("Greasy Fork");
-    greasyForkButton.className = "scr1ptButton scr1ptButton-gold";
-    greasyForkButton.onclick = () => window.open("https://greasyfork.org/es/scripts/36071-superhex-io-scr1pt");
-
-    let unlockSkinsButton = panel.createButton("Unlock skins");
-    unlockSkinsButton.className = "scr1ptButton";
-    unlockSkinsButton.onclick = () => unlockSK();
-
-    let customQualityButton = document.createElement("Button");
-    customQualityButton.setAttribute("class", "scr1ptButton unselected");
-    customQualityButton.setAttribute("type", "button");
-    customQualityButton.setAttribute("id", "btn2");
-    customQualityButton.innerText = "Custom Quality";
-    customQualityButton.onclick = () => changeQ();
-    document.getElementById("button-quality-high").parentElement.appendChild(customQualityButton);
-
-    let versionText = document.createElement("h5");
-    versionText.setAttribute("style", "color: rgba(255,255,255,0.6); position: absolute; bottom: -20px; right: 5px;");
-    versionText.innerText = "v1.9.2";
-    mainPanel.appendChild(versionText);
-
-    let removeAdsCheckbox = panel.createCheckbox("Remove ads")[0]
-    removeAdsCheckbox.onclick = () => toggleRemoveAds(!removeAdsCheckbox.checked);
-    removeAdsCheckbox.checked = AdsTBM;
-
-    let zoomHackCheckbox = panel.createCheckbox("Zoom Hack")[0];
-    zoomHackCheckbox.onclick = () => zoomH(true);
-    zoomHackCheckbox.checked = zoomHack == "True";
-
-    let btnZHS = document.createElement("Button");
-    btnZHS.setAttribute("style", "position: absolute; top: 286px; left: 130px; height: 16px; width: 16px;");
-    btnZHS.setAttribute("class", "scr1ptButton");
-    btnZHS.innerHTML = "<img src='https://lh3.googleusercontent.com/Abm4DjvPOP55GK2MCe9gYh8M1ZJa7ws71oXcW2q6Rl1pQXIQ_bUcVxbN5vZ8_6pmP248O-uQEN2fUxq-xzFlzefdXyEBakvzEgGKzIwSkcdSBHdM2PwtgpgXbMvbP_N7FSI4BYIujg=s16-no' style='position: absolute; left: 0px; top: 0px;'/>";
-    btnZHS.setAttribute("type", "button");
-    btnZHS.setAttribute("id", "btnZHS");
-    btnZHS.onclick = () => setZoomH();
-    mainPanel.appendChild(btnZHS);
-
-    let hotkeysPanel = document.createElement("Div");
-    hotkeysPanel.setAttribute("style", "position: fixed; bottom: -4px; right: -4px; height:150px; width:300px;");
-    hotkeysPanel.setAttribute("class", "scr1ptPanel");
-    hotkeysPanel.setAttribute("id", "scr1ptPanel2");
-    document.getElementById("homepage").appendChild(hotkeysPanel);
-
-    let scrText2 = document.createElement("h4");
-    scrText2.setAttribute("style", "color: white; position: relative; left: 10px;");
-    scrText2.setAttribute("id", "scrText2");
-    scrText2.innerText = "Hotkeys:\n\n1 = Hide/show Leaderboard.\n0 = Hide/show UI.\n2 = Hide/show FPS and connection info.";
-    hotkeysPanel.appendChild(scrText2);
 }
 
 init();
